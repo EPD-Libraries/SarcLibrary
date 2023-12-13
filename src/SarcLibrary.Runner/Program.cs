de7f11d92cd82b2ec11bae8c07d8df2c39831e78
ArchLeaders <archleadership28@gmail.com>
2023-12-12 19:47:07 -0800

    Misc updates

--- src/SarcLibrary.Runner/Program.cs
+++ src/SarcLibrary.Runner/Program.cs
@@ -6,6 +6,7 @@ using SarcLibrary.Runner.Benchmarks;
 BenchmarkRunner.Run<SarcBenchmarks>();
 
 #else
+
 using Revrs;
 using SarcLibrary;
 
@@ -14,11 +15,7 @@ RevrsReader reader = new(buffer);
 ImmutableSarc immutableSarc = new(ref reader);
 Sarc sarc = Sarc.FromImmutable(ref immutableSarc);
 
-foreach ((var name, var data) in immutableSarc) {
-    Console.WriteLine($"{name}: {data.Length}");
-}
-
-using FileStream fs = File.Create("D:\\bin\\Sarc\\Test.pack");
-sarc.Write(fs);
+using MemoryStream ms = new();
+sarc.Write(ms);
 
 #endif
