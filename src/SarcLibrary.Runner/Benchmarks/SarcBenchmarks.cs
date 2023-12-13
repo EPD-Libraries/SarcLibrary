8c2dd9037a36b732166ec4b70ce99e7865ab2c00
ArchLeaders <archleadership28@gmail.com>
2023-12-12 19:47:01 -0800

    Sarc.Write benchmarks

--- src/SarcLibrary.Runner/Benchmarks/SarcBenchmarks.cs
+++ src/SarcLibrary.Runner/Benchmarks/SarcBenchmarks.cs
@@ -7,6 +7,8 @@ public class SarcBenchmarks
 {
     private readonly byte[] _bufferLE = File.ReadAllBytes(@"D:\bin\Sarc\TitleBG-LE.pack");
     private readonly byte[] _bufferBE = File.ReadAllBytes(@"D:\bin\Sarc\TitleBG-BE.pack");
+    private readonly Sarc _sarcLE = Sarc.FromBinary(File.ReadAllBytes(@"D:\bin\Sarc\TitleBG-LE.pack"));
+    private readonly Sarc _sarcBE = Sarc.FromBinary(File.ReadAllBytes(@"D:\bin\Sarc\TitleBG-BE.pack"));
 
     [Benchmark]
     public void Read_LE()
@@ -18,5 +20,19 @@ public class SarcBenchmarks
     public void Read_BE()
     {
         Sarc sarc = Sarc.FromBinary(_bufferBE);
+    }
+
+    [Benchmark]
+    public void Write_LE()
+    {
+        using MemoryStream ms = new();
+        _sarcLE.Write(ms);
+    }
+
+    [Benchmark]
+    public void Write_BE()
+    {
+        using MemoryStream ms = new();
+        _sarcBE.Write(ms);
     }
 }
