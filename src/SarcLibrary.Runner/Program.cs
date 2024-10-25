96aeb2efdc4260f469b7fff55b70a6daf430f527
ArchLeaders <archleadership28@gmail.com>
2024-10-25 13:15:25 -0700

    Update runner

--- src/SarcLibrary.Runner/Program.cs
+++ src/SarcLibrary.Runner/Program.cs
@@ -12,6 +12,9 @@ using SarcLibrary;
 byte[] buffer = File.ReadAllBytes(args[0]);
 Sarc sarc = Sarc.FromBinary(buffer);
 
+using MemoryStream ms = sarc.OpenWrite("Test.txt");
+ms.Write("Test"u8);
+
 using FileStream fs = File.Create(args[1]);
 sarc.Write(fs);
 
