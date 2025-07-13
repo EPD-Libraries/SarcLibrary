f1f77817b1fa067b47bc9b52f040d859848608a3
Amethyst-szs <Amethyst-UwU@protonmail.com>
2025-07-13 08:17:42 -0700

    Fix bfres and byml alignment in SarcAlignment.cs

--- src/Tests/SarcLibrary.Tests/DataProvider.cs
+++ src/Tests/SarcLibrary.Tests/DataProvider.cs
@@ -6,17 +6,29 @@ public static class DataProvider
 {
     public static byte[] GetTest1(Endianness endianness)
     {
-        return File.ReadAllBytes(endianness switch {
+        return File.ReadAllBytes(endianness switch
+        {
+            Endianness.Little => "Data/Test1-LE.sarc",
+            _ => "Data/Test1-BE.sarc"
+        });
+    }
+
+    public static Stream StreamTest1(Endianness endianness)
+    {
+        return File.OpenRead(endianness switch
+        {
             Endianness.Little => "Data/Test1-LE.sarc",
             _ => "Data/Test1-BE.sarc"
         });
     }
     
-    public static Stream StreamTest1(Endianness endianness)
+    
+    public static byte[] GetAirCurrent()
     {
-        return File.OpenRead(endianness switch {
-            Endianness.Little => "Data/Test1-LE.sarc",
-            _ => "Data/Test1-BE.sarc"
-        });
+        return File.ReadAllBytes("Data/AirCurrent.sarc");
+    }
+    public static void WriteGeneratedAirCurrent(MemoryStream stream)
+    {
+        File.WriteAllBytes("Data/AirCurrentOutput.sarc", stream.ToArray());
     }
 }
